46d77ca28ad5aafa179e8e63b13cec0a3aadf6a7
Hüseyin <91679832+huseyinoztrk@users.noreply.github.com>
2023-01-25 15:21:46 +0300

    BlogSite

---
+++ BlogSite/BusinessLayer/Concrete/BlogManager.cs
@@ -0,0 +1,61 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+	public class BlogManager : IBlogService
+	{
+		IBlogDal _blogDal;
+
+		public BlogManager(IBlogDal blogDal)
+		{
+			_blogDal = blogDal;
+		}
+
+		public void BlogAdd(Blog blog)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void BlogDelete(Blog blog)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void BlogUpdate(Blog blog)
+		{
+			throw new NotImplementedException();
+		}
+
+		public List<Blog> GetAllBlogs()
+		{
+			return _blogDal.GetAllList();
+		}
+
+		public List<Blog> GetBlogListWithCategory()
+		{
+			return _blogDal.GetListWithCategory();
+		}
+
+		public Blog GetByID(int id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public List<Blog> GetBlogByID(int id)
+		{
+			return _blogDal.GetListAll(x => x.BlogID == id);
+		}
+
+		public List<Blog> GetBlogListByWriter(int id)
+		{
+			return _blogDal.GetListAll(x => x.WriterID == id);
+		}
+	}
+}
